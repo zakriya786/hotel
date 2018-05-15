5977257e07fa82fa7c9fa41038b54aef80dc232e
zakriya786 <38490116+zakriya786@users.noreply.github.com>
2018-05-15 22:27:57 +0530

    Add files via upload

---
+++ session_m.php
@@ -0,0 +1,18 @@
+<?php
+
+require 'connection.php';
+$conn = Connect();
+
+session_start();
+
+
+$user_check=$_SESSION['login_user1'];
+
+
+$query = "SELECT username FROM MANAGER WHERE username = '$user_check'";
+$ses_sql = mysqli_query($conn, $query);
+$row = mysqli_fetch_assoc($ses_sql);
+$login_session =$row['username'];
+
+
+?>

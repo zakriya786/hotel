5c03e9610d34b3f7201ff90ab22e471e556f1760
zakriya786 <38490116+zakriya786@users.noreply.github.com>
2018-05-15 22:17:28 +0530

    Add files via upload

---
+++ Dessert-Beverage/index.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+include 'connect.php';
+?>
+<html>
+<head>
+ <link rel='stylesheet' href='index.css'>
+<title>hotel</title>
+</head>
+<u><h1><p style="color: #000080;">welcome to hilal hotel</p></h1></u>
+
+<body>
+
+<center>
+<form method='post' action ='authenticate.php'>
+<table align="center">
+<tr><td>Email : </td> <td><input type="email" name="email" maxlength='50' required></td></tr>
+<tr><td>Password : </td> <td><input type="password" name="password" maxlength='50' required></td></td></tr>
+<tr><td colspan='2'><center><button type='Submit' name='login_submit' >Login</button></center></td></tr>
+<tr><td><a href='register.php'>Register</a></td> <td><a href='aboutus.html'>About Us</a></td></tr>
+<tr></tr>
+
+</table>
+</form>
+</center> 
+</body>
+</html>
+
